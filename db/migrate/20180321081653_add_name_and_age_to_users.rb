5dfbd73246215d1a70ecf3ca5fb07799f5a04cf6
Hoang Duc Quan <rocker.gryphon@gmail.com>
2018-03-21 08:44:48 +0000

    fix bug

--- db/migrate/20180321081653_add_name_and_age_to_users.rb
+++ db/migrate/20180321081653_add_name_and_age_to_users.rb
@@ -1,7 +1,6 @@
-class AddNameAndAge < ActiveRecord::Migration
+class AddNameAndAgeToUsers < ActiveRecord::Migration
   def change
     add_column :users, :name, :string
     add_column :users, :age, :integer
-    add_index :users, :name, unique: true
   end
 end
